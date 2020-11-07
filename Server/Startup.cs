f9059a13f30ca27b5bff17cd247d8900578731c6
Collector <collectioner1997@outlook.com>
2020-11-07 20:59:50 +0300

    Added new 'Web' project.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,22 +1,12 @@
-using KeyBoardChat.Web.WebSocketService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace KeyBoardChat
 {
     public class Startup
     {
-
-        private WebSocketService WebSocketService;
-
-        public Startup()
-        {
-
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,15 +22,7 @@ namespace KeyBoardChat
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseFileServer(new FileServerOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Program.FilePath),
-            });
-
-            WebSocketService = new WebSocketService();
-            WebSocketService.Start();
+            app.UseMvc();
         }
     }
 }
