bfb50f1f86c5f71fd8315f769cbf9d0396a5cb8b
Collector <collectioner1997@outlook.com>
2020-11-08 12:48:26 +0300

    Configured front-end build.

---
+++ KeyBoardChat.FrontEnd/.eslintrc.js
@@ -0,0 +1,104 @@
+module.exports = {
+    parser: 'babel-eslint',
+    extends: [
+        'airbnb',
+        'plugin:react-hooks/recommended',
+        'plugin:react/recommended'
+    ],
+
+    rules: {
+        'linebreak-style': ['error', 'windows'],
+        'comma-dangle': ['error', 'never'],
+        'indent': ['error', 4, { SwitchCase: 1 }],
+        'max-len': ['warn', 150, {
+            ignorePattern: '^import\\s.+\\sfrom\\s.+;$'
+        }],
+        'newline-before-return': 'error',
+        'object-curly-newline': ['error', {
+            ObjectExpression: { minProperties: 4, multiline: true, consistent: true },
+            ObjectPattern: { minProperties: 4, multiline: true, consistent: true }
+        }],
+        'arrow-parens': ['error', 'as-needed'],
+        'import/extensions': ['error', {
+            ts: 'never',
+            tsx: 'never'
+        }],
+        'import/no-extraneous-dependencies': 'error',
+        'operator-linebreak': ['error', 'before', {
+            overrides: {
+                '=': 'ignore',
+                '&&': 'ignore'
+            }
+        }],
+        'quote-props': ['error', 'consistent-as-needed'],
+        'array-element-newline': ['error', 'consistent'],
+        'array-bracket-newline': ['error', 'consistent'],
+        'no-case-declarations': 'off',
+        'class-methods-use-this': 'off',
+        'arrow-body-style': 'off',
+        'lines-between-class-members': 'off',
+        'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+        'no-array-constructor': 'off',
+        'no-plusplus': 'off',
+        'no-param-reassign': 'off',
+        'no-confusing-arrow': 'off',
+        'no-mixed-operators': 'off',
+        'no-nested-ternary': 'off',
+        'no-unused-vars': 'off', // used @typescript-eslint rule
+        'semi': 'off', // used @typescript-eslint rule
+        'no-use-before-define': 'off', // used @typescript-eslint rule
+        'no-undef': 'off',
+        'no-underscore-dangle': ['error', { allowAfterThis: true }],
+        'no-multiple-empty-lines': ['error', { max: 2 }],
+
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+        'react/jsx-fragments': ['error', 'element'],
+        'react/static-property-placement': ['error', 'static public field'],
+        'react/jsx-curly-newline': ['error', { multiline: 'require', singleline: 'consistent' }],
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+        'react/no-danger': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/no-array-index-key': 'off',
+        'react/prefer-stateless-function': 'off',
+        'react/sort-comp': 'off',
+        'react/jsx-boolean-value': 'off',
+        'react/jsx-one-expression-per-line': 'off',
+        'react/no-did-update-set-state': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react/state-in-constructor': 'off',
+
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+
+        'jsx-a11y/label-has-associated-control': ['error', {
+            controlComponents: ['ValidationInput']
+        }],
+        'jsx-a11y/anchor-is-valid': 'off',
+
+        'putout/single-property-destructuring': 'error',
+
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-inferrable-types': ['error', {
+            ignoreParameters: true,
+            ignoreProperties: true
+        }],
+        '@typescript-eslint/no-unused-vars': ['error', {
+            vars: 'all',
+            args: 'after-used',
+            ignoreRestSiblings: true
+        }],
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
+    },
+
+    env: {
+        browser: true,
+        node: true
+    }
+}
--- KeyBoardChat.FrontEnd/webpack.config.js
+++ KeyBoardChat.FrontEnd/webpack.config.js
@@ -1,201 +1,113 @@
 const Webpack = require('webpack');
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniSCCExtrractPlugin = require('mini-css-extract-plugin');
+const MiniSCCExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isDev = (/development/).test(process.env.NODE_ENV);
-const outputPath = '../public/';
+
+const outputPath = path.resolve(__dirname, '../KeyBoardChat.Web/wwwroot');
+const resultOutputPath = path.resolve(outputPath, './build');
 
 function generateModule() {
-  return {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react'],
-              plugins: ['react-hot-loader/babel'],
+    return {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-react'],
+                            plugins: ['react-hot-loader/babel']
+                        }
+                    }
+                ]
             },
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniSCCExtrractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  };
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniSCCExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    };
 }
 
 function generatePlugins() {
-  const plugins = [
-    new HTMLWebpackPlugin({
-      template: './source/index.html',
-      favicon: './favicon.ico',
-
-    }),
-    new MiniSCCExtrractPlugin({
-      filename: 'bundle.css',
-    }),
-  ];
-
-  if (!isDev) {
-    plugins.push(
-      new Webpack.IgnorePlugin({
-        resourceRegExp: /fake\.json$/,
-      }),
-    );
-  }
-
-  if (isDev) {
-    plugins.push(
-      new ESLintPlugin({
-        extensions: ['js', 'jsx'],
-        emitWarning: true,
-        failOnError: !isDev,
-      }),
-    );
-  }
-
-  return plugins;
-}
-
-function generateConfig() {
-  return {
-    resolve: {
-      extensions: ['.js', '.jsx'],
-      alias: {
-        shared: path.resolve(__dirname, './source/shared/'),
-        fake_data: path.resolve(__dirname, './fake_data/'),
-        styles: path.resolve(__dirname, './source/styles/'),
-        logic: path.resolve(__dirname, './source/logic/'),
-        components: path.resolve(__dirname, './source/components/'),
-        layouts: path.resolve(__dirname, './source/layouts/'),
-      },
-    },
-
-    mode: isDev ? 'development' : 'production',
-
-    entry: path.resolve(__dirname, './source/index.jsx'),
-
-    module: generateModule(),
-
-    plugins: generatePlugins(),
-
-    devtool: isDev ? 'eval-source-map' : false,
-    devServer: {
-      // open: s,
-      hot: true,
-      contentBase: outputPath,
-      port: 3000,
-      proxy: {
-        '/': 'http://localhost:5000',
-      },
-    },
-
-    output: {
-      path: path.resolve(__dirname, outputPath),
-      filename: 'bundle.js',
-    },
-  };
-}
-
-module.exports = generateConfig();
-
-/* const webpack = require('webpack');
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-/*const isDevelopment = /development/.test(process.env.NODE_ENV);
-
-const APP_NAME = 'Keyboardchat';
-
-function includePlugins() {
-    let plugins = [
-        new htmlWebpackPlugin({
+    const plugins = [
+        new HTMLWebpackPlugin({
             template: './source/index.html',
+            favicon: './favicon.ico'
+
         }),
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
+        new MiniSCCExtractPlugin({
+            filename: 'bundle.css'
         })
     ];
-    //Don't include fake data if you're building production version
-    if (!isDevelopment) {
-        console.log("Ignore fake.json!");
+
+    if (!isDev) {
         plugins.push(
-            new webpack.IgnorePlugin({
-                resourceRegExp: /fake\.json$/,
+            new Webpack.IgnorePlugin({
+                resourceRegExp: /fake\.json$/
             })
-        )
+        );
+    }
+
+    if (isDev) {
+        plugins.push(
+            new ESLintPlugin({
+                extensions: ['js', 'jsx'],
+                emitWarning: true,
+                failOnError: !isDev
+            })
+        );
     }
 
     return plugins;
 }
 
-function generateConfig(output_path) {
+function generateConfig() {
     return {
-        entry: [path.resolve(__dirname, "./source/index.js")],
-
         resolve: {
             extensions: ['.js', '.jsx'],
             alias: {
-                'react-dom': '@hot-loader/react-dom'
-            },
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-react"],
-                        plugins: ["react-hot-loader/babel"],
-                    }
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        'sass-loader',
-                    ],
-                }
-            ]
-        },
-
-        devtool: 'eval-source-map',
-
-        devServer: {
-            contentBase: output_path,
-            port: 3000,
-            hot: true,
-            proxy: {
-                '/': 'http://localhost:5000' //Allows to make request from React.app to server on 4000
+                shared: path.resolve(__dirname, './source/shared/'),
+                fake_data: path.resolve(__dirname, './fake_data/'),
+                styles: path.resolve(__dirname, './source/styles/'),
+                logic: path.resolve(__dirname, './source/logic/'),
+                components: path.resolve(__dirname, './source/components/'),
+                layouts: path.resolve(__dirname, './source/layouts/')
             }
         },
 
-        plugins: includePlugins(),
+        mode: isDev ? 'development' : 'production',
+
+        entry: path.resolve(__dirname, './source/index.jsx'),
+
+        module: generateModule(),
+
+        plugins: generatePlugins(),
+
+        devtool: isDev ? 'eval-source-map' : false,
+        devServer: {
+            // open: s,
+            hot: true,
+            contentBase: outputPath,
+            port: 3000,
+            proxy: {
+                '/': 'http://localhost:5000'
+            }
+        },
+
         output: {
-            path: path.resolve(__dirname, output_path),
-            filename: "bundle.js"
+            path: resultOutputPath,
+            filename: 'bundle.js'
         }
-    }
+    };
 }
 
-function makeOutputPath() {
-    //return path.resolve(__dirname, `./server/WEB pages/${process.env.NODE_ENV}/`);
-    return path.resolve(__dirname, `../public/`);
-}
-
-module.exports = generateConfig(makeOutputPath()); */
+module.exports = generateConfig();
